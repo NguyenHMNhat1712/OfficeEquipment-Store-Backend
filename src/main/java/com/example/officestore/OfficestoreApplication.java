2eb117883cddb6b205c96903034a601c54e595bf
19110254@student.hcmute.edu.vn <19110254@student.hcmute.edu.vn>
2023-08-14 18:42:56 +0700

    Initial commit

---
+++ src/main/java/com/example/officestore/OfficestoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.officestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficestoreApplication.class, args);
+	}
+
+}
